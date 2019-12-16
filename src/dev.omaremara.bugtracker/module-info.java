9b2e9e1a3663e69bb171071caefc172d674c7a76
KarimElWahsh <karimashraf640@gmail.com>
2019-12-16 23:39:59 +0200

    git status

--- src/dev.omaremara.bugtracker/module-info.java
+++ src/dev.omaremara.bugtracker/module-info.java
@@ -2,8 +2,6 @@ module dev.omaremara.bugtracker {
   requires javafx.base;
   requires javafx.controls;
   requires javafx.graphics;
-  requires javax.mail;
-  requires javax.activation;
   exports dev.omaremara.bugtracker to javafx.graphics;
   requires java.sql;
 }
